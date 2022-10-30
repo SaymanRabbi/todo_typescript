0577882db25c0bbde3efd760d1fc1c8e4b714333
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-30 16:42:09 +0600

    edit todo remove todo

--- src/Components/SingelTodo.tsx
+++ src/Components/SingelTodo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import { Todo } from "../model";
@@ -8,6 +9,8 @@ interface Props {
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const SingelTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
   const handelDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -18,15 +21,35 @@ const SingelTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
   const handelDelete = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
+  const handelEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+  const isDoneOrnot = () => {
+    if (!edit && !todo.isDone) {
+      setEdit(!edit);
+    }
+  };
   return (
-    <form className="singel_todo_form">
-      {todo.isDone ? (
+    <form className="singel_todo_form" onSubmit={(e) => handelEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          value={editTodo}
+          type="text"
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todo_singel_text"
+        />
+      ) : todo.isDone ? (
         <s className="todo_singel_text">{todo.todo}</s>
       ) : (
         <span className="todo_singel_text">{todo.todo}</span>
       )}
+
       <div>
-        <span className="icon">
+        <span className="icon" onClick={() => isDoneOrnot()}>
           <AiFillEdit />
         </span>
         <span className="icon" onClick={() => handelDelete(todo.id)}>
