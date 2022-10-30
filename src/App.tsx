b757d8c96cf16cf9fdbde9fba1b311524733dffb
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-30 14:40:54 +0600

    handel add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import Inputfield from "./Components/Inputfield";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div className="App">
       <span className="heading">TodoList</span>
