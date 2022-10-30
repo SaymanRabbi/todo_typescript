1bb67b8c680a8a9fc4df841cacc116bcdbab59b2
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-30 14:19:11 +0600

    style form and input

--- src/Components/Inputfield.tsx
+++ src/Components/Inputfield.tsx
@@ -3,7 +3,9 @@ const Inputfield = () => {
   return (
     <form className="form">
       <input type="text" placeholder="Enter A task" className="input_box" />
-      <button>Go</button>
+      <button className="input_submit" type="submit">
+        Go
+      </button>
     </form>
   );
 };
