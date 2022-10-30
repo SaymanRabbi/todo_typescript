40c76dacbd5c9e21e0fabd08abe7f3c7d56bd27a
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-30 16:04:55 +0600

    todo div style

--- src/Components/SingelTodo.tsx
+++ src/Components/SingelTodo.tsx
@@ -1,3 +1,5 @@
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 import { Todo } from "../model";
 import "./SingelTodo.css";
 interface Props {
@@ -5,8 +7,24 @@ interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
-const SingelTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
-  return <div></div>;
+const SingelTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
+  console.log(todo);
+  return (
+    <form className="singel_todo_form">
+      <span className="todo_singel_text">{todo.todo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingelTodo;
