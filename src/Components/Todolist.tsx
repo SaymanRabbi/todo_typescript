684bfea8eee3aa7f6add4948b01726b34fbb7538
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-30 18:06:04 +0600

    changes some style

--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -7,15 +7,31 @@ interface Props {
 }
 const Todolist: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingelTodo
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="Container">
+      <div className="todos todosactive_task">
+        <span className="todos_heading">Active Task</span>
+
+        {todos.map((todo) => (
+          <SingelTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos complete_task">
+        <span className="todos_heading">Completed Task</span>
+
+        {todos.map((todo) => (
+          <SingelTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
     </div>
   );
 };
