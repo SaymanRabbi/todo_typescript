e638b0f1ff546fd257445b4d13caa90bf45e0730
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-30 15:18:51 +0600

    set todo to todolist

--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,6 +1,11 @@
+import { Todo } from "../model";
 import "./Todolist.css";
-const Todolist = () => {
-  return <div></div>;
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+const Todolist: React.FC<Props> = ({ todos, setTodos }: Props) => {
+  return <div className="todos"></div>;
 };
 
 export default Todolist;
